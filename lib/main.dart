8bf1e21aacc882791636cabeb6a06fb0cc09d8aa
ibrahim-cse <ihrahad233@gmail.com>
2022-01-18 12:44:21 +0600

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
 import 'dart:io';
+
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:path/path.dart' as path;
-import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:intl/intl.dart' show DateFormat;
+import 'package:path/path.dart' as path;
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(MyApp());
@@ -94,7 +95,6 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   borderRadius: BorderRadius.circular(
                     80.0,
-                    // bottom: Radius.elliptical(MediaQuery.of(context).size.width, 100.0),
                   ),
                 ),
                 child: Center(
